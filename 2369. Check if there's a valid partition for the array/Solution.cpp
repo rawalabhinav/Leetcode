06665964b862dfe7d970a87fd69fb316c0ab2086
rawalabhinav <abhinavrawal2002@gmail.com>
2023-08-14 02:13:37 +0530

    2369 Complete

---
+++ 2369. Check if there's a valid partition for the array/Solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef RAWAL
+    #include <Collections/debug.h>
+#endif
+
+class Solution {
+public:
+    bool validPartition(vectorv<int> & nums) {
+        int n = nums.size();
+        if (n == 1) return false;
+
+        vector<bool> dp = {true, false, n > 1 ? nums[0] == nums[1] : false};
+        for (int i = 2; i < n; i++) {
+            bool current_dp = (nums[i] == nums[i - 1] && dp[1]) || 
+                              (nums[i] == nums[i - 1] && nums[i] == nums[i - 2] && dp[0]) ||
+                              (nums[i] - nums[i - 1] == 1 && nums[i - 1] - nums[i - 2] == 1 && dp[0]);
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+            dp[2] = current_dp;
+        }
+        return dp[2];
+    }
+};
