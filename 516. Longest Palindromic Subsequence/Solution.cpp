494b9b14913ba94cc6a79c1e16279c73530e6db2
rawalabhinav <abhinavrawal2002@gmail.com>
2023-09-02 03:45:58 +0530

    516 Complete

--- 516. Longest Palindromic Subsequence/Solution.cpp
+++ 516. Longest Palindromic Subsequence/Solution.cpp
@@ -8,20 +8,13 @@ public:
 
     int help(int i, int j){
         if (i > j) return 0;
-
-        if (i == j) return m[i][j] = 1;
-
         if (m[i][j] != -1) return m[i][j];
 
-        int x = 0,  y = 0, z = 0;
-        if (a[i] == a[j]) x = 2 + help(i + 1, j - 1);
-        else {
-            y = help(i + 1, j);
-            z = help(i, j - 1);
-        }
-
-        return m[i][j] = max(x, max(y, z));
+        if (i == j) return m[i][j] = 1;
+        if (a[i] == a[j]) return m[i][j] = 2 + help(i + 1, j - 1);
+        return m[i][j] = max(help(i + 1, j), help(i, j - 1));
     }
+
     int longestPalindromeSubseq(string s) {
         a = s;
         int n = s.size();
