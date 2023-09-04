d44df8fadfc2bd48cd70048e6989cdca87a87faa
rawalabhinav <abhinavrawal2002@gmail.com>
2023-09-04 14:43:15 +0530

    141 Complete

---
+++ 141. Linked List Cycle/Solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef RAWAL
+    #include <Collections/debug.h>
+#endif
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    const int smol = -1 -1e5;
+    bool hasCycle(ListNode *head) {
+        for(; head != NULL; head = head -> next){
+            if (head -> val == smol){
+                return true;
+            }
+
+            head -> val = smol;
+        }
+
+        return false;
+    }
+}; 
