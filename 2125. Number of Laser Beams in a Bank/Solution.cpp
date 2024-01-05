0548a3075c16c018fded03d49bba0003b6a93bfd
rawalabhinav <abhinavrawal2002@gmail.com>
2024-01-05 13:47:24 +0530

    2125 Complete

--- 2125. Number of Laser Beams in a Bank/Solution.cpp
+++ 2125. Number of Laser Beams in a Bank/Solution.cpp
@@ -1,20 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifdef RAWAL
-    #include <Collections/debug.h>
-#endif
+class Solution {
+public:
+    int numberOfBeams(vector<string>& a) {
+        int n = a.size(), m = a[0].size();
 
-#define int long long int
+        int ans = 0, cur = 0, prev = 0;
+        for(int i = 0; i < n; ++i){
+            for(char& c: a[i]){
+                cur += (c == '1');
+            }
 
-void solve(){
-    
-}
+            ans += cur * prev;
+            if (cur) {
+                prev = cur;
+                cur = 0;
+            }
+        }
 
-signed main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t; cin >> t;
-    while(t--) solve();
-}
+        return ans;
+    }
+};
