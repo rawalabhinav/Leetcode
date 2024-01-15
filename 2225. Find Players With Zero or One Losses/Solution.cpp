330eb1d3b489213cfb8dc87bf321bec6ce799865
rawalabhinav <abhinavrawal2002@gmail.com>
2024-01-15 22:14:47 +0530

    Template Created

---
+++ 2225. Find Players With Zero or One Losses/Solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef RAWAL
+    #include <Collections/debug.h>
+#endif
+
+#define int long long int
+
+void solve(){
+    
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while(t--) solve();
+}
